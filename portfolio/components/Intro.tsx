570b4a9a0cfaf5f7a3898f16d754dd37a8e15d65
Hemraj <mememimi413@gmail.com>
2024-08-26 16:40:51 +0545

    character fixed

--- portfolio/components/Intro.tsx
+++ portfolio/components/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
         <main className='h-[100vh] w-full flex flex-row-reverse sm:flex-row justify-center items-center gap-[3vw] xl:scale-95  md:scale-90 sm:scale-75 scale-[.8] intro' id='intro'>
             <div className="bg-[url('/profile.jpg')] sm:w-[300px] sm:h-[300px] h-[200px] w-[200px] rounded-full   bg-center bg-cover shadow-xl shadow-green-800 self-center"> </div>
             <div className="">
-                <h3 className='sm:text-3xl text-xl font-mono '>Hii <span className='text-purple-600'>There!</span><span>I'm </span></h3>
+                <h3 className='sm:text-3xl text-xl font-mono '>Hii <span className='text-purple-600'>There!</span><span>I&apos;m </span></h3>
                 <h1 className='sm:text-5xl text-2xl font-bold text-purple-700'>Himal Bhusal</h1>
                 <p className='sm:max-w-[40vw] xl:max-w-[30vw] sm:text-2xl w-[50vw]'>Frontend web developer passionate about creating interactive web pages.</p>
                 <section className="flex sm:flex-row flex-col  max-w-fit gap-6 my-5">
